6e6c990cece38086ec3a428333b45b7d9e5d0506
Clemens Stift <clemens@stift.online>
2018-06-04 11:17:26 +0200

    Changed input param type to [String: Any] from [Any] and addressing fields in citybike JSON.

--- CityBikeApp/BikeStation.swift
+++ CityBikeApp/BikeStation.swift
@@ -1,9 +1,7 @@
 //
 //  Artwork.swift
-//  HonoluluArt
 //
 //  Created by Clemens Stift on 28.05.18.
-//  Copyright © 2018 Ray Wenderlich. All rights reserved.
 //
 
 import Foundation
@@ -12,60 +10,52 @@ import Contacts
 
 class BikeStation: NSObject, MKAnnotation {
     let title: String?
-    let locationName: String
-    let discipline: String
+    let freeBikes: Int
+    var locationDescription: String
     let coordinate: CLLocationCoordinate2D
+    let freeBoxesForReturn: Int
     
-    init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
+    init(title: String, freeBikes: Int, freeBoxesForReturn: Int, locationDescription: String, coordinate: CLLocationCoordinate2D) {
         self.title = title
-        self.locationName = locationName
-        self.discipline = discipline
+        self.freeBikes = freeBikes
+        self.locationDescription = locationDescription
         self.coordinate = coordinate
+        self.freeBoxesForReturn = freeBoxesForReturn
         
         super.init()
     }
     
     init?(bikeStationDictionary: [String: Any]) {
         self.title = bikeStationDictionary["name"] as? String ?? "No Title"
-        self.locationName = bikeStationDictionary["description"] as! String
-        self.discipline = bikeStationDictionary["status"] as! String
-        let latitude = bikeStationDictionary["longitude"] as! Double
-        let longitude = bikeStationDictionary["latitude"] as! Double
+        self.freeBikes = bikeStationDictionary["free_bikes"] as! Int
+        self.locationDescription = bikeStationDictionary["description"] as! String
+        self.freeBoxesForReturn = bikeStationDictionary["free_boxes"] as! Int
+        let latitude = bikeStationDictionary["latitude"] as! Double
+        let longitude = bikeStationDictionary["longitude"] as! Double
+        
+        if self.locationDescription.isEmpty {
+            self.locationDescription = "No location description available."
+        }
         
         self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        
-        debugPrint(self.coordinate)
-        
     }
     
-    // TODO Change this to return a different location name.
     var subtitle: String? {
-        return locationName
+        return  self.locationDescription + "\n\n" + String(self.freeBoxesForReturn) + " empty slots for returning bikes."
     }
     
-    // TODO Change this to reflect different levels of free bikes.
     var markerTintColor: UIColor  {
-        switch discipline {
-        case "Monument":
+        if freeBikes == 0 {
             return .red
-        case "Mural":
-            return .cyan
-        case "Plaque":
-            return .blue
-        case "Sculpture":
-            return .purple
-        default:
+        } else if freeBikes <= 3 {
+            return .yellow
+        } else {
             return .green
         }
     }
     
-    var imageName: String? {
-        if discipline == "Sculpture" { return "Statue" }
-        return "Flag"
-    }
-    
     func mapItem() -> MKMapItem {
-        let addressDict = [CNPostalAddressStreetKey: subtitle!]
+        let addressDict = [CNPostalAddressStreetKey: title!]
         let placemark = MKPlacemark(coordinate: coordinate, addressDictionary: addressDict)
         let mapItem = MKMapItem(placemark: placemark)
         mapItem.name = title
