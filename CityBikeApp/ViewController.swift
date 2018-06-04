6f227e0c9724a182f7f26527badcd863db0ad327
Clemens Stift <clemens@stift.online>
2018-06-04 11:19:09 +0200

    Changed AnnotationView back to MarkerAnnotationView.

--- CityBikeApp/ViewController.swift
+++ CityBikeApp/ViewController.swift
@@ -9,19 +9,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Initial location Stephansdom.
         let initialLocation = CLLocation(latitude: 48.208415, longitude: 16.371282)
         centerMapOnLocation(location: initialLocation)
         
         mapView.delegate = self
-        //        mapView.register(ArtworkMarkerView.self,
-        //                         forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
-        
-        mapView.register(BikeStationView.self,
+        mapView.register(BikeStationMarkerView.self,
                          forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
         
         loadInitialData()
-        
-        debugPrint(bikestations)
         
         mapView.addAnnotations(bikestations)
         
@@ -35,7 +31,6 @@ class ViewController: UIViewController {
     }
     
     func loadInitialData() {
-        debugPrint("I'm in the function.")
         guard let fileName = Bundle.main.path(forResource: "Citybikes", ofType: "json")
             else {return}
         let optionalData = try? Data(contentsOf: URL(fileURLWithPath: fileName))
@@ -45,40 +40,35 @@ class ViewController: UIViewController {
             let json = try? JSONSerialization.jsonObject(with: data),
             let stations = json as? [[String: Any]]
         
-            else { debugPrint("Shit happened."); return}
-        
-        debugPrint("List of stations now:")
-        debugPrint(stations)
+            else { debugPrint("Took a wrong turn at handling the json data."); return}
         
         var validStations = [BikeStation]()
         for station in stations {
-            var typeCastedStation = BikeStation(bikeStationDictionary: station)
-            validStations.append(typeCastedStation!)
+            validStations.append(BikeStation(bikeStationDictionary: station)!)
         }
         
         bikestations.append(contentsOf: validStations)
-        
     }
     
 }
 
 extension ViewController: MKMapViewDelegate {
-    //    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-    //        guard let annotation = annotation as? Artwork else { return nil }
-    //        let identifier = "marker"
-    //        var view: MKMarkerAnnotationView
-    //        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
-    //            as? MKMarkerAnnotationView {
-    //            dequeuedView.annotation = annotation
-    //            view = dequeuedView
-    //        } else {
-    //            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-    //            view.canShowCallout = true
-    //            view.calloutOffset = CGPoint(x: -5, y: 5)
-    //            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-    //        }
-    //        return view
-    //    }
+//        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//            guard let annotation = annotation as? BikeStation else { return nil }
+//            let identifier = "marker"
+//            var view: MKMarkerAnnotationView
+//            if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+//                as? MKMarkerAnnotationView {
+//                dequeuedView.annotation = annotation
+//                view = dequeuedView
+//            } else {
+//                view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+//                view.canShowCallout = true
+//                view.calloutOffset = CGPoint(x: -5, y: 5)
+//                view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+//            }
+//            return view
+//        }
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView,
                  calloutAccessoryControlTapped control: UIControl) {
